e4389d74f76845e039dc93bc5611599469059660
Candela Landi <landicandela01@gmail.com>
2024-06-24 15:04:59 -0300

    updating angular.v

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoModule } from './todos/todo.module';
+import { TodoAddComponent } from './todos/todo-add/todo-add.component';
+import { TodoFooterComponent } from './todos/todo-footer/todo-footer.component';
+import { TodoItemComponent } from './todos/todo-item/todo-item.component';
+import { TodoListComponent } from './todos/todo-list/todo-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
